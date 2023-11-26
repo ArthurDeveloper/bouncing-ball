b25d5bfa8549b5c56641bfe3feb780cd3ed02302
ArthurDev <arthurdevoficial@gmail.com>
2023-11-26 16:10:46 -0300

    Added bounce to all walls and fixed bounce angles

--- sketch.js
+++ sketch.js
@@ -8,20 +8,22 @@ const ball = {
 		y: 0,
 	},
 	acceleration: {
-		x: 150,
+		x: 10,
 		y: G,
 	},
 	radius: 40,
+	angleOfMovement: 60 * (Math.PI / 180),
 	update(dt) {
-		this.speed.x += this.acceleration.x * dt;
-		this.speed.y += this.acceleration.y * dt;
+		const acceleration = Math.sqrt(Math.pow(this.acceleration.x, 2) + Math.pow(this.acceleration.y, 2));
+		this.speed.x += Math.cos(this.angleOfMovement) * acceleration * dt;
+		this.speed.y += Math.sin(this.angleOfMovement) * acceleration * dt;
 
-		this.x += this.speed.x * dt;
-		this.y += this.speed.y * dt;
+		const speed = Math.sqrt(Math.pow(this.speed.x, 2) + Math.pow(this.speed.y, 2));
+		this.x += Math.cos(this.angleOfMovement) * speed * dt;
+		this.y += Math.sin(this.angleOfMovement) * speed * dt;
 	},
 	bounce() {
-		this.speed.x = this.speed.x;
-		this.speed.y = -this.speed.y;
+		this.angleOfMovement = this.angleOfMovement + 90;
 	}
 }
 
@@ -34,11 +36,28 @@ function draw() {
 	fill('#fff');
 
 	ball.update(deltaTime / 1000);
-	if (ball.y + 2*ball.radius > windowHeight) {
+	if (ball.x + 2*ball.radius > windowWidth)	{
+		ball.x = windowWidth - 2*ball.radius;
+		ball.bounce();
+	} else if ((ball.y + 2*ball.radius > windowHeight)) {
+		ball.y = windowHeight - 2*ball.radius;
+		ball.bounce();
+	} else if (ball.y < 0) {
+		ball.y = 0;
+		ball.bounce();
+	} else if (ball.x < 0) {
+		ball.x = 0;
 		ball.bounce();
 	}
 
 	circle(ball.x, ball.y, 2*ball.radius);
+	stroke(255, 0, 0);
+	line(
+		ball.x, 
+		ball.y, 
+		ball.x + Math.cos(ball.angleOfMovement) * 100,
+		ball.y + Math.sin(ball.angleOfMovement) * 100
+	);
 }
 
 function windowResized() {
